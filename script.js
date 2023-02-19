49516f1d2c14998cf78436547ad357a8eb9a2c39
Jerrard Rolle-Stewart <114176897+KingKazmaa@users.noreply.github.com>
2023-02-19 02:17:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// start counter at 0
+const currentNum = 0
+
+
+// if the mouse is clicked
+if (Input.getKey(keyCode.Mouse0)) {
+  // execute increase or decrease
+}
+
+//update the display every time a button is clicked
+
+
+//increase counter (function)
+const increaseNum = () 
+  => currentNum += 1
+
+//decrease counter (function)
+const decreaseNum = () 
+  => currentNum -= 1
